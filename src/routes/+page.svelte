3e109db452dff58c36542f4ac73fe1cba0b9415c
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2024-03-06 16:18:01 +0100

    adsf

---
+++ src/routes/+page.svelte
@@ -0,0 +1,266 @@
+<script>
+	import Modal from '$lib/Modal.svelte';
+
+	let modalOpen = false;
+</script>
+
+<button on:click={() => (modalOpen = !modalOpen)}>show video</button>
+
+{#if modalOpen}
+	<Modal onClose={() => (modalOpen = false)}>
+		<iframe
+			src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		></iframe>
+	</Modal>
+{/if}
+
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
+<p>
+	stug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd
+	fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug
+	asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd fasfstug asdf ads asd fasd
+	fasfstug asdf ads asd fasd fasf
+</p>
